b5790764a806a674971c2749a8a4b82f9697d6be
willi123yao <willi123yao@gmail.com>
2020-03-10 20:56:24 +0800

    Show last updated time placeholder

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,12 @@ class Footer extends React.Component {
     return (
       <div className="bz-footer uk-position-bottom">
         <div className="uk-container">
-          Made with <span className="uk-icon uk-icon-image" style={{backgroundImage: `url(${Heart})`}}></span> by <a className="bz-author" href="https://github.com/willi123yao">willi123yao</a>
+          <div style={{display: 'inline-block'}}>
+            Made with <span className="uk-icon uk-icon-image" style={{backgroundImage: `url(${Heart})`}}></span> by <a className="bz-author" href="https://github.com/willi123yao">willi123yao</a>
+          </div>
+          <div className="uk-align-right" style={{display: 'inline-block'}}>
+            Last updated: 10 March 2020 12:52 UTC
+          </div>
         </div>
       </div>
     );
