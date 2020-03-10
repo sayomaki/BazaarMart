c9503f7dd5bca824ea3f72eca71bcd32382fdb19
willi123yao <willi123yao@gmail.com>
2020-03-10 20:49:22 +0800

    Add the various sections

--- src/App.js
+++ src/App.js
@@ -24,6 +24,49 @@ function App() {
         </div>
       </div>
 
+      <div className="bz-body uk-container uk-margin-top uk-margin-bottom">
+        <ul uk-accordion="collapsible: false">
+          <li>
+            <a class="uk-accordion-title" href="#">
+              <span className="bz-section-title bz-title-farming">Farming</span>
+            </a>
+            <div class="uk-accordion-content">
+              <p>COMING SOON!</p>
+            </div>
+          </li>
+          <li>
+            <a class="uk-accordion-title" href="#">
+              <span className="bz-section-title bz-title-mining">Mining</span></a>
+            <div class="uk-accordion-content">
+              <p>COMING SOON!</p>
+            </div>
+          </li>
+          <li>
+            <a class="uk-accordion-title" href="#">
+            <span className="bz-section-title bz-title-combat">Combat</span></a>
+            <div class="uk-accordion-content">
+              <p>COMING SOON!</p>
+            </div>
+          </li>
+          <li>
+            <a class="uk-accordion-title" href="#">
+              <span className="bz-section-title bz-title-woodfish">Woods & Fishes</span>
+            </a>
+            <div class="uk-accordion-content">
+              <p>COMING SOON!</p>
+            </div>
+          </li>
+          <li class="uk-open">
+            <a class="uk-accordion-title" href="#">              
+              <span className="bz-section-title bz-title-oddities">Oddities</span>
+            </a>
+            <div class="uk-accordion-content">
+              
+            </div>
+          </li>
+        </ul>
+      </div>
+
       <Footer/>
     </div>
   );
