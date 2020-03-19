97136d4a42bd7eb23bfeac65b3fe24d994e9b973
willi123yao <willi123yao@gmail.com>
2020-03-19 16:45:07 +0800

    Added all slayer items in oddities

--- src/Oddities.js
+++ src/Oddities.js
@@ -18,6 +18,36 @@ class Oddities extends React.Component {
             </thead>
             <tbody>
               <tr>
+                <td className="bz-color-oddities">Revenant Flesh</td>
+                <td className="bz-item-price">{this.props.items["REVENANT_FLESH"] ? this.props.items["REVENANT_FLESH"].buyPrice : 'N/A'}</td>
+                <td className="bz-item-price">{this.props.items["REVENANT_FLESH"] ? this.props.items["REVENANT_FLESH"].sellPrice : 'N/A'}</td>
+              </tr>
+              <tr>
+                <td className="bz-color-oddities">Revenant Viscera</td>
+                <td className="bz-item-price">{this.props.items["REVENANT_VISCERA"] ? this.props.items["REVENANT_VISCERA"].buyPrice : 'N/A'}</td>
+                <td className="bz-item-price">{this.props.items["REVENANT_VISCERA"] ? this.props.items["REVENANT_VISCERA"].sellPrice : 'N/A'}</td>
+              </tr>
+              <tr>
+                <td className="bz-color-oddities">Tarantula Web</td>
+                <td className="bz-item-price">{this.props.items["TARANTULA_WEB"] ? this.props.items["TARANTULA_WEB"].buyPrice : 'N/A'}</td>
+                <td className="bz-item-price">{this.props.items["TARANTULA_WEB"] ? this.props.items["TARANTULA_WEB"].sellPrice : 'N/A'}</td>
+              </tr>
+              <tr>
+                <td className="bz-color-oddities">Tarantula Silk</td>
+                <td className="bz-item-price">{this.props.items["TARANTULA_SILK"] ? this.props.items["TARANTULA_SILK"].buyPrice : 'N/A'}</td>
+                <td className="bz-item-price">{this.props.items["TARANTULA_SILK"] ? this.props.items["TARANTULA_SILK"].sellPrice : 'N/A'}</td>
+              </tr>
+              <tr>
+                <td className="bz-color-oddities">Wolf Tooth</td>
+                <td className="bz-item-price">{this.props.items["WOLF_TOOTH"] ? this.props.items["WOLF_TOOTH"].buyPrice : 'N/A'}</td>
+                <td className="bz-item-price">{this.props.items["WOLF_TOOTH"] ? this.props.items["WOLF_TOOTH"].sellPrice : 'N/A'}</td>
+              </tr>
+              <tr>
+                <td className="bz-color-oddities">Golden Tooth</td>
+                <td className="bz-item-price">{this.props.items["GOLDEN_TOOTH"] ? this.props.items["GOLDEN_TOOTH"].buyPrice : 'N/A'}</td>
+                <td className="bz-item-price">{this.props.items["GOLDEN_TOOTH"] ? this.props.items["GOLDEN_TOOTH"].sellPrice : 'N/A'}</td>
+              </tr>
+              <tr>
                 <td className="bz-color-oddities">Stock of Stonks</td>
                 <td className="bz-item-price">{this.props.items["STOCK_OF_STONKS"] ? this.props.items["STOCK_OF_STONKS"].buyPrice : 'N/A'}</td>
                 <td className="bz-item-price">{this.props.items["STOCK_OF_STONKS"] ? this.props.items["STOCK_OF_STONKS"].sellPrice : 'N/A'}</td>
