8d8893e4157993df5f6bb41d0405d2a021159e00
willi123yao <willi123yao@gmail.com>
2020-05-14 23:38:36 +0800

    Add credits to HSB Fandom Wiki and Minecraft Wiki

--- src/Footer.js
+++ src/Footer.js
@@ -11,9 +11,11 @@ class Footer extends React.Component {
     return (
       <div className="bz-footer">
         <div className="uk-container">
-          <div style={{display: 'inline-block'}}>
+          <div className="bz-footer-credits">
             Made with <span className="uk-icon uk-icon-image" style={{backgroundImage: `url(${Heart})`}}></span> by <a className="bz-author" href="https://github.com/willi123yao">willi123yao</a>.
-            Data courtesy of <a className="bz-author" href="https://hypixel.net">Hypixel</a>
+            Data courtesy of <a className="bz-author" href="https://hypixel.net">Hypixel</a>.
+            <br/>
+            Icon credits to <a className="bz-author" href="https://hypixel-skyblock.fandom.com">Hypixel Skyblock Fandom Wiki</a> and <a className="bz-author" href="https://minecraft.gamepedia.com/">Minecraft Wiki</a>
           </div>
           <div className="uk-align-right" style={{display: 'inline-block'}}>
             Last updated: {this.parseDate()}
